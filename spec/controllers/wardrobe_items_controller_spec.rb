12b0f5f7e81eb92564151fbfa21a6b8d03bbbc03
Rainbough Phillips <rainbough@gmail.com>
2013-09-30 11:57:02 -0500

    add test for wardrobe item controller show method

--- spec/controllers/wardrobe_items_controller_spec.rb
+++ spec/controllers/wardrobe_items_controller_spec.rb
@@ -6,8 +6,21 @@ describe WardrobeItemsController do
 		wardrobe_item = WardrobeItem.create
 		get :index
 
+		expect(assigns('wardrobe_items')).to eq([wardrobe_item])
+
 		end
 
+	end
+
+	describe 'GET show' do
+		it "assign individual wardrobe item to @wardrobe_item" do
+			wardrobe_item = WardrobeItem.create
+			
+			get :show, {:id => wardrobe_item}
+
+			expect(assigns('wardrobe_item')).to eq(wardrobe_item)
+
+		end
 	end
 
 end
